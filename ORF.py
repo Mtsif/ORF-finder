0cb48b31f3cc2f293ad67e905f179eca522b9cd8
Tsifintaris Margaritis <101420482+Mtsif@users.noreply.github.com>
2023-07-06 18:31:08 +0300

    Update ORF.py

--- ORF.py
+++ ORF.py
@@ -7,6 +7,8 @@ while True:
     print("--------------------------------------------")
     valid_seq = "ATCGatcg"
     coding = ""
+    
+    # Check if the sequence contains only valid characters (A, T, C, G)
     check = all(n in valid_seq for n in seq)
 
     if seq == "0":
@@ -16,17 +18,22 @@ while True:
         print("--------------------------------------------")
         print("The sequence does NOT contain only the specified characters [A,T,C,G] \n")
     else:   
+        # Find the start codons (ATG) in the sequence
         start = [m.start() for m in re.finditer("ATG", seq)]
+        
+        # Find the stop codons (TAG, TAA, TGA) in the sequence
         TAG = [m.start() for m in re.finditer("TAG", seq)]
         TAA = [m.start() for m in re.finditer("TAA", seq)]
         TGA = [m.start() for m in re.finditer("TGA", seq)]
+        
+        # Combine the stop codons and sort them
         stop = TAG + TAA + TGA
         stop.sort()
 
-        
         for x in range(0, len(start)):
             for y in range(0, len(stop)):
                 if stop[y] > start[x]:
+                    # Check if the distance between start and stop codons is a multiple of 3
                     if (stop[y]-start[x]) >3 and ((stop[y]-start[x]) % 3) == 0:
                         print(">", seq[start[x]:stop[y]+3])
                         coding = "yes"
@@ -38,7 +45,3 @@ while True:
         else:
             print("--------------------------------------------")
             print("The sequence does not include coding regions \n")
-
-
-
-
